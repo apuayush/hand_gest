fad52aeaa88a73f841c4028681d7888f9599fb49
apuayush <apuayush@gmail.com>
2018-03-25 18:30:43 +0530

    working on multiple pools

--- tf_hand/main.py
+++ tf_hand/main.py
@@ -1,4 +1,4 @@
-from tf_hand.utils import detector_utils as detector_utils
+from utils import detector_utils as detector_utils
 import cv2
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -6,6 +6,8 @@ import multiprocessing
 from multiprocessing import Queue, Pool
 import time
 import datetime
+
+frame_processed = 0
 
 def worker(input_q, output_q, cap_params, frame_processed):
     print(">> loading frozen model for worker")
@@ -20,7 +22,8 @@ def worker(input_q, output_q, cap_params, frame_processed):
                 frame, detection_graph, sess)
             # draw bounding boxes
             detector_utils.draw_box_on_image(
-                cap_params['num_hands_detect'], cap_params["score_thresh"], scores, boxes, cap_params['im_width'], cap_params['im_height'], frame)
+                cap_params['num_hands_detect'], cap_params["score_thresh"], scores, boxes, cap_params['width']
+                , cap_params['height'], frame)
             output_q.put(frame)
             frame_processed += 1
         else:
@@ -30,15 +33,18 @@ def worker(input_q, output_q, cap_params, frame_processed):
 
 if __name__ == "__main__":
 
+    cap_params = {'width': 640, 'height': 380}
+    frame_processed = 0
+
+    # Video configs
     cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, cap_params['width'])
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_params['height'])
+
     input_q = Queue(maxsize=10)
     output_q = Queue(maxsize=10)
 
-    cap_params = {}
-    frame_processed = 0
-    fps = 0
-
-    cap_params['im_width'], cap_params['im_height'] = 640, 380
+    # cap_params['im_width'], cap_params['im_height'] = cam.get()
     cap_params['score_thresh'] = 0.3
 
     # max number of hands we want to detect/track
@@ -49,7 +55,7 @@ if __name__ == "__main__":
 
     # To parallelize detection
     pool = Pool(
-        5, worker, (input_q, output_q, cap_params, frame_processed)
+        4, worker, (input_q, output_q, cap_params, frame_processed)
     )
 
     while True:
@@ -61,8 +67,13 @@ if __name__ == "__main__":
         input_q.put(fram_rgb)
         output_frame = output_q.get()
 
+        output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
+
+        cv2.imshow("hand tracking", output_frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    pool.terminate()
     cam.release()
     cv2.destroyAllWindows()
